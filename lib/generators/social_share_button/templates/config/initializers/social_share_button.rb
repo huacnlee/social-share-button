44ca24cac32716be200ca8baccb3665ba8869844
Nikolay Moskvin <moskvin@sibext.com>
2014-01-24 11:10:41 +0700

    Added new social network - vk.com. Added to facebook description and image

--- lib/generators/social_share_button/templates/config/initializers/social_share_button.rb
+++ lib/generators/social_share_button/templates/config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq kaixin001 baidu google_bookmark delicious huaban tumblr plurk)
+  config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq kaixin001 baidu google_bookmark delicious huaban tumblr plurk vk)
 end
