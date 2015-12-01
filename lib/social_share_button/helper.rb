fc745e2e6412381d00b11c926a9afc4db3da5c67
Luka Novsak <lnovsak@gmail.com>
2015-12-01 12:51:21 +0100

    allow for override of allowed sites in helper

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -8,7 +8,7 @@ module SocialShareButton
       html << "<div class='social-share-button' data-title='#{h title}' data-img='#{opts[:image]}'"
       html << "data-url='#{opts[:url]}' data-desc='#{opts[:desc]}' data-popup='#{opts[:popup]}' data-via='#{opts[:via]}'>"
 
-      SocialShareButton.config.allow_sites.each do |name|
+      (opts[:allow_sites] || SocialShareButton.config.allow_sites).each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
         special_data = opts.select { |k, _| k.to_s.start_with?('data-' + name) }
 
