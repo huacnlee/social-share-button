d3f0a954cf9b1b3dd3ed1de9f01a7097355d4834
Rich Daley <rich@fishpercolator.co.uk>
2017-09-25 14:54:43 +0100

    Add a text_on_button option to show text on the button instead of in the title

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -8,7 +8,8 @@ module SocialShareButton
       html = []
       html << "<div class='social-share-button' data-title='#{h title}' data-img='#{opts[:image]}'"
       html << "data-url='#{opts[:url]}' data-desc='#{opts[:desc]}' data-via='#{opts[:via]}'>"
-
+      text_on_button = opts[:text_on_button] || false
+      
       opts[:allow_sites].each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
         special_data = opts.select { |k, _| k.to_s.start_with?('data-' + name) }
@@ -16,11 +17,11 @@ module SocialShareButton
         special_data["data-wechat-footer"] = t "social_share_button.wechat_footer" if name == "wechat"
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
-        html << link_to("", "#", { :rel => ["nofollow", rel],
+        html << link_to((text_on_button ? link_title : ""), "#", { :rel => ["nofollow", rel],
                                    "data-site" => name,
-                                   :class => "ssb-icon ssb-#{name}",
+                                   :class => "ssb-icon ssb-#{name}#{' ssb-text-button' if text_on_button}",
                                    :onclick => "return SocialShareButton.share(this);",
-                                   :title => h(link_title) }.merge(extra_data).merge(special_data))
+                                   :title => text_on_button ? nil : h(link_title) }.merge(extra_data).merge(special_data))
       end
       html << "</div>"
       raw html.join("\n")
